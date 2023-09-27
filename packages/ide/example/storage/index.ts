b60bf54221236e10066da2a816b5704a44f94b25
陈华春 <samchen08@163.com>
2023-09-27 16:24:21 +0800

    0.6.4

--- packages/ide/example/storage/index.ts
+++ packages/ide/example/storage/index.ts
@@ -1,18 +1,23 @@
 /// <reference types="vite/client" />
-const modules = import.meta.glob(['/.vtj/project/*.json', '/.vtj/file/*.json']);
+const modules = import.meta.glob([
+  '/.vtj/project/*.json',
+  '/.vtj/file/*.json',
+  '/src/views/pages/*.vue',
+  '/src/components/blocks/*.vue'
+]);
 export default {
   modules,
   service: 'file',
-  raw: false,
+  raw: true,
   isProd: process.env.NODE_ENV === 'production',
   project: {
     id: 'ide',
-    name: 'IDE',
+    name: '项目样例',
     base: './',
     mode: 'hash',
     page: '/page',
     preview: '/preview',
-    home: '/'
+    home: '/dashboard'
   },
   ide: { path: '/ide/' }
 };
