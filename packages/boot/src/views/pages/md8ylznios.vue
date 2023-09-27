dd606d7ad1c59f7edbd30a241954086202e01c4a
陈华春 <samchen08@163.com>
2023-09-27 19:19:05 +0800

    replace出码模板绑定数据this

--- packages/boot/src/views/pages/md8ylznios.vue
+++ packages/boot/src/views/pages/md8ylznios.vue
@@ -12,25 +12,39 @@
       src="/ide/assets/logo-32adf494.png">
     </XIcon>
   </ElContainer>
+  <ElDivider> 分割线 </ElDivider>
+  <XIcon icon="GoodsFilled" :size="100" color="#F10B0B"> </XIcon>
+  <XIcon
+    :icon="state.icon"
+    :size="100"
+    color="#F10B0B"
+    @click="click_handler_2jl6c2bv8q8">
+  </XIcon>
 </template>
 <script lang="ts">
   import { defineComponent, reactive } from 'vue';
-  import { ElContainer } from 'element-plus';
+  import { ElContainer, ElDivider } from 'element-plus';
   import { XIcon } from '@vtj/ui';
   import { useProvider } from '@vtj/runtime';
   export default defineComponent({
     name: 'Icon',
 
-    components: { ElContainer, XIcon },
+    components: { ElContainer, XIcon, ElDivider },
 
     setup(props) {
       const provider = useProvider();
-      const state = reactive({});
+      const state = reactive({ icon: 'Plus' });
       return {
         state,
         props,
         provider
       };
+    },
+
+    methods: {
+      click_handler_2jl6c2bv8q8() {
+        this.state.icon = 'Minus';
+      }
     }
   });
 </script>
