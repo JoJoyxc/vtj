3e4a836646caf2bffc2985dc4d39cbfddf470b79
陈华春 <samchen08@163.com>
2024-01-24 21:30:24 +0800

    fix:源码生成引用 $libs.VtjUtils 没有被替换前缀

--- packages/coder/tests/index.test.ts
+++ packages/coder/tests/index.test.ts
@@ -2,6 +2,11 @@ import { expect, test } from 'vitest';
 
 import { generator } from '../src';
 
-test('index', () => {
+// import { deps } from './deps';
+// import { dsl } from './dsl';
+
+test('index', async () => {
+  // const content = await generator(dsl as any, new Map(), deps);
+  // console.log(content);
   expect(!!generator).toBeTruthy();
 });
