bf24af0b520c00662c1aeaabf7d71745c9867101
陈华春 <samchen08@163.com>
2024-05-01 09:59:42 +0800

    ci: 👷commit

--- packages/designer/src/components/editor/index.ts
+++ packages/designer/src/components/editor/index.ts
@@ -1,14 +1,17 @@
-import { defineAsyncComponent, defineComponent, h } from 'vue';
-export default defineAsyncComponent({
-  loader: () => import('./Editor.vue'),
-  loadingComponent: defineComponent({
-    render() {
-      return h('div', '正在拼命加载...');
-    }
-  }),
-  errorComponent: defineComponent({
-    render() {
-      return h('div', '组件加载失败！');
-    }
-  })
-});
+// import { defineAsyncComponent, defineComponent, h } from 'vue';
+import Editor from './Editor.vue';
+export default Editor;
+
+// export default defineAsyncComponent({
+//   loader: () => import('./Editor.vue'),
+//   loadingComponent: defineComponent({
+//     render() {
+//       return h('div', '正在拼命加载...');
+//     }
+//   }),
+//   errorComponent: defineComponent({
+//     render() {
+//       return h('div', '组件加载失败！');
+//     }
+//   })
+// });
