6ff9fd524b6b5cad32c012b672955abd840a138c
陈华春 <samchen08@163.com>
2024-05-07 09:44:31 +0800

    ci: 👷commit

--- packages/charts/src/version.ts
+++ packages/charts/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, VTJ.PRO All rights reserved.
  * @name @vtj/charts 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.8.22
+ * @version 0.8.23
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.8.22';
+export const version = '0.8.23';
