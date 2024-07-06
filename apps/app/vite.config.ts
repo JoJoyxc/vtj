27321c9f45fcc6fbc3fdb7cfabb2175084db89dd
陈华春 <samchen08@163.com>
2024-07-06 08:38:45 +0800

    ci: 👷commit

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,7 +1,8 @@
 import { createViteConfig } from '@vtj/cli';
 import { createDevTools } from '@vtj/pro/vite';
 import proxy from './proxy.config';
-const basePath = '/lowcode/';
+// const basePath = '/lowcode/';
+const basePath = '/';
 export default createViteConfig({
   base: basePath,
   proxy,
