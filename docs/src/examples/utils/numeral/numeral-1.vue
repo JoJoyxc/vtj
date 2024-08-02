52e87a0a5178fe3ed39fd1ab2129a253aeb45064
chenxiaohui <cxh5760863@dingtalk.com>
2024-08-02 15:18:11 +0800

    docs: ✏️  utils 数字格式化

---
+++ docs/src/examples/utils/numeral/numeral-1.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <table border="1" style="width: 100%" align="center">
+      <caption>
+        字符串转换例子
+      </caption>
+      <thead>
+        <tr>
+          <th scope="col">函数名</th>
+          <th scope="col" style="text-align: center">例子</th>
+          <th scope="col" style="text-align: center">返回值</th>
+        </tr>
+      </thead>
+      <tbody align="center">
+        <tr>
+          <th scope="row">numberFormat</th>
+          <td class="code">numberFormat(124.665)</td>
+          <td>{{ numberFormat(124.665) }}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">numberFormat</th>
+          <td class="code">numberFormat(124.123, '0.0')</td>
+          <td>{{ numberFormat(124.123, '0.0') }}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">numberFormat</th>
+          <td class="code">numberFormat(124.123,'0.000')</td>
+          <td>{{ numberFormat(124.123, '0.000') }}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">toFixed</th>
+          <td class="code">toFixed(12.166, 2, true)</td>
+          <td>{{ toFixed(12.166, 2, true) }}</td>
+        </tr>
+
+        <tr>
+          <th scope="row">toFixed</th>
+          <td class="code">toFixed(12.166, 2, false)</td>
+          <td>{{ toFixed('12.166', 2, false) }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { numberFormat, toFixed } from '@vtj/utils';
+</script>
+
+<style scoped>
+  table {
+    border-collapse: collapse;
+    border: 2px solid rgb(140 140 140);
+    font-family: sans-serif;
+    letter-spacing: 1px;
+    font-size: 14px;
+  }
+
+  caption {
+    caption-side: top;
+    padding: 10px;
+    font-weight: bold;
+    font-size: 22px;
+  }
+
+  thead,
+  tfoot {
+    background-color: rgb(228 240 245);
+  }
+
+  th {
+    border: 1px solid rgb(160 160 160);
+    padding: 4px;
+  }
+  td {
+    border: 1px solid rgb(160 160 160);
+    padding: 4px;
+  }
+
+  td:last-of-type {
+    text-align: center;
+    font-weight: bolder;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .code {
+    letter-spacing: 1px;
+    font-size: 16px;
+    font-style: italic;
+  }
+</style>
---
+++ docs/src/utils/base/numeral.md
@@ -0,0 +1,17 @@
+# numeral 数字格式化  
+
+
+
+| 函数名       | 描述                          | 类型     | 参数                                                                       | 返回值   |
+| ------------ | ----------------------------- | -------- | -------------------------------------------------------------------------- | -------- |
+| numberFormat | 数字格式化，默认2位小数       | function | (`val:number`, `format<0.00>?:string`)                                     | `number` |
+| toFixed      | 保留小数点，默认2位           | function | (数值`val:number`, 小数位数`format<2>:number`,是否四舍五入`round:boolean`) | `number` |
+| numeral      | [文档](http://numeraljs.com/) | -        | -                                                                          | -        |
+
+
+
+### 示例
+
+:::preview
+demo-preview=../../examples/utils/numeral/numeral-1.vue
+:::
