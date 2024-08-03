fb6cb4a5c599613ab100cca42a46e204634dc14f
陈华春 <samchen08@163.com>
2024-08-03 16:31:23 +0800

    docs: ✏️  项目集成文档

--- docs/.vitepress/sidebar/guide.ts
+++ docs/.vitepress/sidebar/guide.ts
@@ -5,6 +5,7 @@ export default [
     items: [
       { text: '简介', link: '/' },
       { text: '快速上手', link: '/start' },
+      { text: '项目集成', link: '/integration' },
       { text: '功能扩展', link: '/extension' },
       {
         text: '打造自己专属的低代码平台',
@@ -13,17 +14,17 @@ export default [
     ]
   },
   {
-    text: '基础功能',
+    text: '基础',
     base: '/guide/base',
     items: [
       { text: '概念', link: '/concept' },
-      { text: '设计器界面模块', link: '/designer' },
-      { text: '页面管理', link: '/page' },
-      { text: '区块管理', link: '/block' }
+      { text: '创建一个低代码应用（ Todo ）', link: '/todo' },
+      { text: '项目工程模板', link: '/project-template' },
+      { text: '设计器使用手册', link: '/designer' }
     ]
   },
   {
-    text: '进阶主题',
+    text: '进阶',
     base: '/guide/extras',
     items: [
       { text: '低代码物料制作', link: '/material' },
