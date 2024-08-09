623a77e3aefc6847e539404220c51dc48ca15c51
陈华春 <samchen08@163.com>
2024-08-09 09:11:20 +0800

    ci: 👷commit

--- packages/core/src/version.ts
+++ packages/core/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, VTJ.PRO All rights reserved.
  * @name @vtj/core 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.8.99
+ * @version 0.8.100
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.8.99';
+export const version = '0.8.100';
