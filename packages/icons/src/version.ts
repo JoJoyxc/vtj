91a6d222b9fc06fd0abd3b3fac1e3f35747794a1
陈华春 <samchen08@163.com>
2024-08-22 11:29:10 +0800

    ci: 👷commit

--- packages/icons/src/version.ts
+++ packages/icons/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, VTJ.PRO All rights reserved.
  * @name @vtj/icons 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.8.110
+ * @version 0.8.111
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.8.110';
+export const version = '0.8.111';
