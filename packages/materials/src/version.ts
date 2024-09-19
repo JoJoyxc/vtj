15d0ba6531e58b1d9e58ede8d809b2225c4f71c7
陈华春 <samchen08@163.com>
2024-09-19 16:00:13 +0800

    ci: 👷commit

--- packages/materials/src/version.ts
+++ packages/materials/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, VTJ.PRO All rights reserved.
  * @name @vtj/materials 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.8.143
+ * @version 0.8.144
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.8.143';
+export const version = '0.8.144';
