e339950c6bdf263bb1d09ff6363d3f1776d7001b
“chenhuachun” <samchen08@163.com>
2024-11-18 20:36:32 +0800

    fix: 🐛 vtj link

--- platforms/pro/public/entry/inject-devtools.js
+++ platforms/pro/public/entry/inject-devtools.js
@@ -7,7 +7,8 @@ function injectLink() {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="m199.04 672.64 193.984 112 224-387.968-193.92-112-224 388.032zm-23.872 60.16 32.896 148.288 144.896-45.696zM455.04 229.248l193.92 112 56.704-98.112-193.984-112-56.64 98.112zM104.32 708.8l384-665.024 304.768 175.936L409.152 884.8h.064l-248.448 78.336zm384 254.272v-64h448v64h-448z"></path></svg>';
   panel.appendChild(link);
   link.addEventListener('click', function (e) {
-    const section = window.location.hash.split('?')[0].split('/');
+    const { hash, pathname } = location;
+    const section = hash ? hash.split('?')[0].split('/') : pathname.split('/');
     const id = section.pop() || '';
     const options = window.__VTJ_LINK__ || {};
     let path = options.href || window.location.pathname + '__vtj__/#/';
