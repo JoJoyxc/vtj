d48d2b46a12a9fee2d145b819ab398cfecc2e528
“chenhuachun” <samchen08@163.com>
2024-11-19 18:04:23 +0800

    feat: ✨ base add arrayToTree

---
+++ dev/src/views/utils/data.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>data</div>
+</template>
+<script lang="ts" setup>
+  import { arrayToTree } from '@vtj/utils';
+
+  const tree = arrayToTree([
+    { id: 1, name: 'a', parentId: null },
+    { id: 2, name: 'a-1', parentId: 1 },
+    { id: 3, name: 'b', parentId: null },
+    { id: 4, name: 'b-1', parentId: 3 },
+    { id: 5, name: 'a-1-1', parentId: 2 }
+  ]);
+
+  console.log(tree);
+</script>
--- packages/base/src/data.ts
+++ packages/base/src/data.ts
@@ -1,3 +1,4 @@
+import { groupBy } from './lodash';
 /**
  * 数组转Map
  * @param data
@@ -231,4 +232,27 @@ export function splitParser(val?: string, flag: string = ',') {
  */
 export function splitStringify(val?: string[], flag: string = ',') {
   return val ? val.join(flag) : '';
+}
+
+/**
+ * 数组转换为树结构
+ * @param array
+ * @param id
+ * @param parentId
+ * @returns
+ */
+export function arrayToTree<T extends Record<string, any>>(
+  array: T[],
+  id: keyof T = 'id',
+  parentId: keyof T = 'parentId',
+  children: string = 'children',
+  root: string | number = 'null'
+) {
+  const groupByParentId = groupBy(array, parentId);
+  for (const items of Object.values(groupByParentId)) {
+    items.forEach((item: any) => {
+      item[children] = groupByParentId[item[id]] || [];
+    });
+  }
+  return groupByParentId[root];
 }
