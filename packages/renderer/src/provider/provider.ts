8a3a1373136816039f43554135d13445aa7b4f58
“chenhuachun” <samchen08@163.com>
2024-12-05 14:47:08 +0800

    fix: 🐛 provider

--- packages/renderer/src/provider/provider.ts
+++ packages/renderer/src/provider/provider.ts
@@ -143,6 +143,8 @@ export class Provider extends Base {
     // 设计模式在引擎已初始化了项目数据，这里不需要再次初始化
     if (project && mode !== ContextMode.Design) {
       this.load(project as ProjectSchema);
+    } else {
+      this.project = project as ProjectSchema;
     }
   }
 
