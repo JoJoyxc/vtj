3df34c342e5fefd193d5a0ad5c3713ead9d3204c
“chenhuachun” <samchen08@163.com>
2024-12-05 10:27:40 +0800

    ci: 👷commit

--- packages/renderer/src/version.ts
+++ packages/renderer/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, VTJ.PRO All rights reserved.
  * @name @vtj/renderer 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.9.0-alpha.25
+ * @version 0.9.0-alpha.26
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.9.0-alpha.25';
+export const version = '0.9.0-alpha.26';
