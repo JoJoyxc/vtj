b5e02099bb4a6ac09c707872f17fda120a62fc0b
“chenhuachun” <samchen08@163.com>
2024-12-16 13:34:41 +0800

    ci: 👷commit

--- packages/coder/src/version.ts
+++ packages/coder/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, VTJ.PRO All rights reserved.
  * @name @vtj/coder 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.9.0-alpha.47
+ * @version 0.9.0-alpha.48
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.9.0-alpha.47';
+export const version = '0.9.0-alpha.48';
