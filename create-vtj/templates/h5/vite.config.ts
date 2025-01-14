eb22ff065d2c16d3d6e5f7c150178007c535d51d
“chenhuachun” <samchen08@163.com>
2025-01-14 14:09:08 +0800

    fix: 🐛 h5项目模版

--- create-vtj/templates/h5/vite.config.ts
+++ create-vtj/templates/h5/vite.config.ts
@@ -1,17 +1,7 @@
 import { createViteConfig } from '@vtj/cli';
 import { createDevTools } from '@vtj/pro/vite';
 import proxy from './proxy.config';
-// const basePath = '/lowcode/';
-const basePath = '/';
 export default createViteConfig({
-  base: basePath,
   proxy,
-  elementPlus: false,
-  plugins: [
-    createDevTools({
-      staticBase: basePath,
-      devMode: true,
-      pluginNodeModulesDir: '../../node_modules'
-    })
-  ]
+  plugins: [createDevTools()]
 });
