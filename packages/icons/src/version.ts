67c3860fa1479cf939b64f585559e9584e9d025a
“chenhuachun” <samchen08@163.com>
2025-01-14 11:36:48 +0800

    ci: 👷commit

--- packages/icons/src/version.ts
+++ packages/icons/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2025, VTJ.PRO All rights reserved.
  * @name @vtj/icons 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.9.17
+ * @version 0.9.18
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.9.17';
+export const version = '0.9.18';
