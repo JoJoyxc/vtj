9768d44029718290bf8700deef01830d5d49b95e
“chenhuachun” <samchen08@163.com>
2025-01-14 15:53:37 +0800

    ci: 👷commit

--- packages/materials/src/version.ts
+++ packages/materials/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2025, VTJ.PRO All rights reserved.
  * @name @vtj/materials 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.9.19
+ * @version 0.9.20
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.9.19';
+export const version = '0.9.20';
