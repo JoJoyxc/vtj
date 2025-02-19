74a103b9567b753b10b9c7d16ca1775949302163
“chenhuachun” <samchen08@163.com>
2025-02-19 15:45:19 +0800

    ci: 👷commit

--- packages/parser/src/version.ts
+++ packages/parser/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2025, VTJ.PRO All rights reserved.
  * @name @vtj/parser 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.9.25
+ * @version 0.10.0
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.9.25';
+export const version = '0.10.0';
