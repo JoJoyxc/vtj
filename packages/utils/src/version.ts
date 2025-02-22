6b2646ab0ca27cf24e19d2d2151df4fe513822c7
“chenhuachun” <samchen08@163.com>
2025-02-22 09:07:22 +0800

    ci: 👷commit

--- packages/utils/src/version.ts
+++ packages/utils/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2025, VTJ.PRO All rights reserved.
  * @name @vtj/utils 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.10.1-alpha.1
+ * @version 0.10.1-alpha.2
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.10.1-alpha.1';
+export const version = '0.10.1-alpha.2';
