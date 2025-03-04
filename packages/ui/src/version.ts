e2583ab23d7b1867b2335fc81b877b36963f5f40
“chenhuachun” <samchen08@163.com>
2025-03-04 08:40:34 +0800

    ci: 👷commit

--- packages/ui/src/version.ts
+++ packages/ui/src/version.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2025, VTJ.PRO All rights reserved.
  * @name @vtj/ui 
  * @author CHC chenhuachun1549@dingtalk.com 
- * @version 0.10.6
+ * @version 0.10.7
  * @license <a href="https://vtj.pro/license.html">MIT License</a>
  */
-export const version = '0.10.6';
+export const version = '0.10.7';
